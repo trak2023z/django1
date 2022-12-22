0a8b7313e0ee28bfbef85be8fce9f86062d7020b
trak2023z <113351512+trak2023z@users.noreply.github.com>
2022-12-22 21:22:21 +0100

    c2

---
+++ announcements/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from announcements.models import *
+
+
+# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Announcement)
--- announcements/apps.py
+++ announcements/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AnnoucementsConfig(AppConfig):
+class AnnouncementsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "annoucements"
+    name = "announcements"

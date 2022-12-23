9851eb5e5445d578fc90e15f2aa36351eb27e822
trak2023z <113351512+trak2023z@users.noreply.github.com>
2022-12-23 22:47:12 +0100

    c4

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -39,6 +39,13 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "announcements.apps.AnnouncementsConfig",
+    "rest_framework",
+    "rest_framework.authtoken",
+    'dj_rest_auth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth',
+    'dj_rest_auth.registration',
 ]
 
 MIDDLEWARE = [
